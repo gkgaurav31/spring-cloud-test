d1b7611311472315ee099a030865feeaa62724d7
gkgaurav31 <gkgaurav31@gmail.com>
2019-11-04 23:13:38 +0530

    TesT

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -2,15 +2,23 @@ package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
+import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
 import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserRequestModel;
+import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserResponseModel;
 
 @RestController
 @RequestMapping("/users")
@@ -19,14 +27,37 @@ public class UsersController {
 	@Autowired
 	Environment env;
 	
-	@GetMapping(path="/status/check")
-	public String status() {
-		return "Running on port : " + env.getProperty("local.server.port");
+	@Autowired
+	UsersService usersService;
+	
+	private Environment environment;
+	
+	@Autowired
+	public UsersController(Environment environment) {
+		this.environment = environment;
 	}
 	
-	@PostMapping
-	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
-		return "Create user method is called";
+	@GetMapping(path="/status/check")
+	public String status() {
+		return "Running on port : " + env.getProperty("local.server.port") + ". Config Server says: " + environment.getProperty("messageFromConfigServer");
+	}
+	
+	@PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+			produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+			)
+	public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+		System.out.println("createUser called");
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		
+		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+		
+		System.out.println("calling create user");
+		UserDto createdUser = usersService.createUser(userDto);
+		
+		CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
 	}
 	
 }
